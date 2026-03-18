091b906760555b9f5202f9d9371069a015ed7b9e
s36080 <s36080@pjwstk.edu.pl>
2026-03-18 13:14:33 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
 public class Main {
      public static void main(String[] args) {
 
-         książka harryPotter = new książka ("harryPotter", "JK Rowling,", 300, true);
+         książka harryPotter = new książka("harryPotter", "JK Rowling,", 300, true);
 
 
          harryPotter.wypiszinfo();
          harryPotter.wypozycz();
          harryPotter.zwroc();
+
+         System.out.println("--------------------");
+
+
+         książka quovadis = new książka ("quo vadis", "Henryk Sienkiewicz", 500, false);
+
+         quovadis.wypiszinfo();
+         quovadis.wypozycz();
+         quovadis.zwroc();
+
+         System.out.println("--------------------");
+
+         książka pantadeusz = new książka ("pan tadeusz", "adam mickiewicz", 700, true);
+
+         pantadeusz.wypiszinfo();
+         pantadeusz.wypozycz();
+         pantadeusz.zwroc();
+
+
+
      }
 }
